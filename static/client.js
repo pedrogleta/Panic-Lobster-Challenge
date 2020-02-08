ef4e883044bfcedd70890370bc8013f62945eb78
Pedro Gonçalves Leta <pedrogleta@gmail.com>
2020-02-08 19:40:42 -0300

    states and cities working

--- static/client.js
+++ static/client.js
@@ -1,1 +1,17 @@
-alert('hello there')
+function changeCities(){
+    var Estado = $('select')[0].value;
+    var state = states.filter(state => state.Estado == Estado);
+    var uf = state[0].UF;
+    console.log(uf);
+    var citySel = $('select')[1];
+    while(citySel.firstChild){
+        citySel.removeChild(citySel.firstChild);
+    };
+    cities.forEach(function(city){
+        if (city.UF == uf){
+            var opt = document.createElement('option');
+            opt.appendChild(document.createTextNode(city.Município));
+            citySel.appendChild(opt);
+        };
+    })
+}
