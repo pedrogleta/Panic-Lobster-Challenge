253fb0a74190b1939df13955d0a2c3816efed5ea
Pedro Gonçalves Leta <pedrogleta@gmail.com>
2020-02-08 20:46:50 -0300

    final input touches

--- static/client.js
+++ static/client.js
@@ -2,7 +2,6 @@ function changeCities(){
     var Estado = $('select')[0].value;
     var state = states.filter(state => state.Estado == Estado);
     var uf = state[0].UF;
-    console.log(uf);
     var citySel = $('select')[1];
     while(citySel.firstChild){
         citySel.removeChild(citySel.firstChild);
@@ -14,4 +13,36 @@ function changeCities(){
             citySel.appendChild(opt);
         };
     })
-}
+};
+
+// (function($) {
+//     $.fn.inputFilter = function(inputFilter) {
+//       return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+//         if (inputFilter(this.value)) {
+//           this.oldValue = this.value;
+//           this.oldSelectionStart = this.selectionStart;
+//           this.oldSelectionEnd = this.selectionEnd;
+//         } else if (this.hasOwnProperty("oldValue")) {
+//           this.value = this.oldValue;
+//           this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+//         } else {
+//           this.value = "";
+//         }
+//       });
+//     };
+//   }(jQuery));
+
+//   $(document).ready(function() {
+//     $("#cpf").inputFilter(function(value) {
+//       return /^\d{11}$/g.test(value);    // Allow digits only, using a RegExp
+//     });
+//   });
+
+$('#cpf').keyup(function(e)
+                                {
+  if (!(/\d/.test(this.value) || /-/.test(this.value) || /\./.test(this.value)))
+  {
+    // Filter non-digits from input value.
+    this.value = this.value.replace(/\D/g, '');
+  }
+});
