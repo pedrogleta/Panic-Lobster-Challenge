9d3b8d41f3ddcfc3e13ccc4dfcdf5de8d455a407
Pedro Gonçalves Leta <pedrogleta@gmail.com>
2020-02-09 12:02:55 -0300

    name & email length bug fix

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.post('/list/new', function(req, res){
     })
 })
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, process.env.IP, function(){
     console.log('Server Started');
 });
